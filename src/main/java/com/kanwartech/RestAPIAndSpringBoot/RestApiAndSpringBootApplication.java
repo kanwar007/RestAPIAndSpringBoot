598f0d3a32fdf67ec99b5305c7bbae1d90d18261
vijaykanwar <vijayblp.kanwar@gmail.com>
2024-08-28 08:58:46 +0800

    commit the porject

---
+++ src/main/java/com/kanwartech/RestAPIAndSpringBoot/RestApiAndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kanwartech.RestAPIAndSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAndSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanwartech/RestAPIAndSpringBoot/RestApiAndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanwartech.RestAPIAndSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
